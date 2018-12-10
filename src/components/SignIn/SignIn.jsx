23af15d669ceadb900e83537e77585102e5d0fe7
DanielKanyo <danielkanyog@gmail.com>
2018-12-10 13:38:03 +0100

    Better look for the sign in component

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -2,6 +2,33 @@ import React, { Component } from 'react';
 import { auth } from '../Firebase';
 import { Link } from "react-router-dom";
 
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  paper: {
+    padding: '8px 16px'
+  },
+  textField: {
+    marginTop: 10,
+    width: '100%'
+  },
+  dense: {
+    marginTop: 19,
+  },
+  button: {
+    marginTop: 10,
+    width: '100%'
+  }
+});
+
 class SignIn extends Component {
 
   constructor(props) {
@@ -31,27 +58,44 @@ class SignIn extends Component {
   }
 
   render() {
+    const { classes } = this.props;
+
     return (
       <div className="component-content">
         <form onSubmit={this.handleSubmit}>
-          <label>
-            E-mail:
-            <input type="text" value={this.state.email} onChange={(e) => { this.handleChange(e, 'email') }} />
-          </label>
-          <br />
-          <label>
-            Password:
-            <input type="password" value={this.state.password} onChange={(e) => { this.handleChange(e, 'password') }} />
-          </label>
-          <br />
-          <input type="submit" value="Sign In" />
-          <div>
-            <Link to="/signUp/">SignUp</Link>
-          </div>
+          <Paper className={classes.paper} elevation={1}>
+            <TextField
+              id="email-textfield"
+              label="E-mail"
+              className={classes.textField}
+              value={this.state.email}
+              onChange={(e) => { this.handleChange(e, 'email') }}
+              margin="normal"
+            />
+            <TextField
+              id="password-textfield"
+              label="Password"
+              className={classes.textField}
+              value={this.state.password}
+              onChange={(e) => { this.handleChange(e, 'password') }}
+              margin="normal"
+              type="password"
+            />
+            <Button className={classes.button} variant="contained" color="primary" type="submit">
+              SIGN IN
+            </Button>
+            <div className="sign-up">
+              <Link to="/signUp/">Sign Up</Link>
+            </div>
+          </Paper>
         </form>
       </div>
     );
   }
 }
 
-export default SignIn;
+SignIn.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SignIn);
